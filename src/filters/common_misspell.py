6e92d101b7c713efa20e57f5fb8db4b319fc3255
Changwoo Ryu <cwryu@debian.org>
2002-07-17 18:15:36 +0000

    common wrong spelling checks

---
+++ src/filters/common_misspell.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import re
+
+name = "common-misspell"
+
+misspell_data = [
+    { 're':    re.compile("(않\s*함)"),
+      'error': "\"%s\": '않'이 아니라 '안'입니다" },
+    { 're':    re.compile("(읍니다)"),
+      'error': "\"%s\": '읍니다'가 아니라 '습니다'입니다" }
+]
+
+def check(msgid,msgstr):
+    ret = 1
+    errmsg = ""
+    for data in misspell_data:
+        misspell_re = data['re']
+        misspell_error = data['error']
+        str = msgstr
+        while 1:
+            print str
+            mo = misspell_re.search(str)
+            if mo:
+                ret = 0
+                if errmsg:
+                    errmsg += '\n'
+                errmsg += misspell_error % str[mo.start(1):mo.end(1)]
+                str = str[mo.end():]
+            else:
+                break;
+    return (ret, errmsg)    
+
+if __name__ == '__main__':
+    import sys
+    msgid = sys.stdin.readline()
+    msgstr = sys.stdin.readline()
+    t,e = check(msgid,msgstr)
+    if not t:
+        print e
+    else:
+        print "Success"
