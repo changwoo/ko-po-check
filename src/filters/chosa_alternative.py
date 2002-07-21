c97cc153d52499d7435e3118b86020199b5b598e
Changwoo Ryu <cwryu@debian.org>
2002-07-21 07:02:35 +0000

    corrected err message

--- src/filters/chosa_alternative.py
+++ src/filters/chosa_alternative.py
@@ -4,8 +4,8 @@ import re
 
 name = "should-have-chosa-alternative"
 
-chosa_re = re.compile("(%[cdfs](이|가|을|를|로서|로써|로|으로서|으로써|으로|로써))")
-error_string = "\"%s\": 조사에 따른 받침 구별이 없습니다" 
+chosa_re = re.compile("(%[cdfs][\"\']?(이|가|을|를|은|는|로서|로써|로|으로서|으로써|으로|로써))")
+error_string = "\"%s\": 받침에 따른 조사 구별(예: %%s을(를))이 없습니다" 
 def check(msgid,msgstr):
     ret = 1
     errmsg = ""
