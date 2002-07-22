e1608b410cc3696fb48f509bf407d1510f5a8db2
Changwoo Ryu <cwryu@debian.org>
2002-07-22 06:43:33 +0000

    라는/이라는 추가

--- src/filters/chosa_alternative.py
+++ src/filters/chosa_alternative.py
@@ -4,7 +4,7 @@ import re
 
 name = "should-have-chosa-alternative"
 
-chosa_re = re.compile("(%([1-9]\$)?[cdfs][\"\']?(이|가|을|를|은|는|로서|로써|로|으로서|으로써|으로|로써))")
+chosa_re = re.compile("(%([1-9]\$)?[cdfs][\"\']?(이라는|라는|이|가|을|를|은|는|로서|로써|로|으로서|으로써|으로|로써))")
 error_string = "\"%s\": 받침에 따른 조사 구별(예: %%s을(를))이 없습니다" 
 def check(msgid,msgstr):
     ret = 1
