b77c944dc656ab723c1ccc46d4f43f930c8d18ed
Changwoo Ryu <cwryu@debian.org>
2002-07-23 15:04:01 +0000

    impoved chosa regexp

--- src/filters/chosa_alternative.py
+++ src/filters/chosa_alternative.py
@@ -4,7 +4,10 @@ import re
 
 name = "should-have-chosa-alternative"
 
-chosa_re = re.compile("(%([1-9]\$)?[cdfs][\"\']?(이라는|라는|이|가|을|를|은|는|로서|로써|로|으로서|으로써|으로|로써))")
+format = "%([1-9][0-9]*\$)?[-+ #'0]*([1-9][0-9]*)?(\.[0-9]+)?((hh|h|j|l|L|ll|q|t|z|Z)?[dioufeEgGaAcCspnmhjlLqtzZ1-9]|hh|ll)"
+chosa = "(이라는|라는|이|가|을|를|은|는|로서|로써|로|으로서|으로써|으로|로써)"
+
+chosa_re = re.compile("("+format+chosa+")")
 error_string = "\"%s\": 받침에 따른 조사 구별(예: %%s을(를))이 없습니다" 
 def check(msgid,msgstr):
     ret = 1
