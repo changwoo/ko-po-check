5419f179a777e8113286dfb2fa2be326be248266
Changwoo Ryu <cwryu@debian.org>
2002-07-25 18:25:15 +0000

    added "copyright", "gnome_glossary" filters

---
+++ src/filters/copyright.py
@@ -0,0 +1,30 @@
+import re, string
+
+name = "copyright"
+
+copyright_re = re.compile(r"^[Cc]opyright \([Cc]\)")
+
+def euc(s):
+    return unicode(s,'euc-kr').encode('utf-8')
+
+error_string = euc("copyright notice는 번역하면 안 됩니다")
+
+def check(msgid,msgstr):
+    msgid_lines = string.split(msgid,"\n")
+    msgstr_lines = string.split(msgstr,"\n")
+    lineno = 0
+    for line in msgid_lines:
+        if copyright_re.match(line) and line != msgstr_lines[lineno]:
+            return (0,error_string)
+        lineno+=1
+    return (1,"")
+
+if __name__ == '__main__':
+    import sys
+    msgid = sys.stdin.readline()
+    msgstr = sys.stdin.readline()
+    t,e = check(msgid,msgstr)
+    if not t:
+        print e
+    else:
+        print "Success"
