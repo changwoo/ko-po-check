3724c04427cdf8ad5afe7f9b2cdd0670c215656c
Changwoo Ryu <cwryu@debian.org>
2002-07-27 15:25:57 +0000

    get the most recent messages

--- misc/get-tp-po.sh
+++ misc/get-tp-po.sh
@@ -1,18 +1,28 @@
 #!/bin/sh
+
 LISTHTML=list.html
 LIST=list.txt
 DIR=po-tp
-URL="http://www.iro.umontreal.ca/contrib/po/teams/PO/ko/"
+DIRURL="http://www.iro.umontreal.ca/contrib/po/teams/PO/ko/"
+URL="http://www.iro.umontreal.ca/contrib/po/teams/PO/ko/?M=D"
 
 rm -rf $DIR
 mkdir $DIR
 
 wget -O $LISTHTML $URL
-grep "HREF=.*ko.po" $LISTHTML | sed -e "s/^.*\"\(.*ko.po\)\".*$/\1/" >$LIST
+grep "HREF=.*ko.po" $LISTHTML | sed -e "s/^.*\"\(.*ko.po\)\".*$/\1/" > $LIST
 rm -f $LISTHTML
 
 for F in `cat $LIST`; do
-    wget -O $DIR/$F $URL/$F
+    DOMAIN=`echo $F|sed -e "s/^\(.*\)-.*$/\1/"`
+    if eval "test x\$HAS_$DOMAIN != x1"; then
+	echo "*** Getting $F..."
+	wget -O $DIR/$F $DIRURL/$F
+    else
+	echo "*** Skipping old $F..."
+    fi
+    eval "HAS_$DOMAIN=1"
 done
+
 rm $LIST
 
