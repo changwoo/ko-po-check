e438d4d091c30445fccfe097a0aba617a8a709eb
Changwoo Ryu <cwryu@debian.org>
2002-08-04 09:35:55 +0000

    fixed unicode side effect

--- src/filters/gnome.py
+++ src/filters/gnome.py
@@ -9,7 +9,7 @@ error_string = e("GNOME에서 \"translator_credits\"에는 번역자들의 이름을 써야 합
 
 def check(msgid,msgstr):
     if (msgid[:18] == "translator_credits" and
-        (msgstr[:18] == "translator_credits" or msgstr[:6] == e("번역"))):
+        (msgstr[:18] == "translator_credits" or msgstr[:2] == e("번역"))):
         return (0,error_string)
     return (1,"")
 
