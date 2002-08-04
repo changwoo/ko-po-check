c529ecb57854d30049171fcd8e2785c9b8f9a6be
Changwoo Ryu <cwryu@debian.org>
2002-08-04 07:09:05 +0000

    * src/filters/kde.py: Applied the KDE patch from KIM KyungHeon.

--- src/filters/kde.py
+++ src/filters/kde.py
@@ -5,10 +5,13 @@ name = "kde"
 import string,localeutil
 e = localeutil.eucstr
 
-error1_string = e("KDE에서 번역자들의 이름을 써야 합니다.  \"_:\"은 안 써도 됩니다")
-error2_string = e("KDE에서 번역자들의 이메일을 써야 합니다  \"_:\"은 안 써도 됩니다")
+error0_string = e("KDE에서 \"_:\"부터 첫 번째 \\n까지는 본디말에 대한 설명이므로 번역하면 안 됩니다.")
+error1_string = e("번역자 이름을 써야 합니다.  \"_:\"로 시작하는 문장은 번역하면 안 됩니다.")
+error2_string = e("번역자 이메일을 써야 합니다  \"_:\"로 시작하는 문장은 번역하면 안 됩니다.")
 
 def check(msgid,msgstr):
+    if (msgstr[:3] == "_: "):
+        return (0,error0_string)
     if (msgid[:22] == "_: NAME OF TRANSLATORS" and
         (msgstr[:3] == "_: " or string.find(msgstr,e("번역")) >= 0)):
         return (0,error1_string)
