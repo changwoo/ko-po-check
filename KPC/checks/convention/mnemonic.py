9a6d53eabd881c89db117b4b6f1a1dc42d7d0c60
Changwoo Ryu <cwryu@debian.org>
2008-11-22 06:17:45 +0000

    Added more exception to the mnemonic check

--- KPC/checks/convention/mnemonic.py
+++ KPC/checks/convention/mnemonic.py
@@ -6,8 +6,14 @@ name = 'convention/mnemonic'
 description = "접근키를 올바로 번역했는지 검사합니다"
 
 re_mnemonic = re.compile('^[^_]*[_&]([0-9A-Za-z])[^_]*$')
-re_mnemonic_unlikely = re.compile('(^[^_]*[a-zA-Z]_[A-Z][^_]*$|^translator_credits.*|.*%[ds].*)')
 group_mnemonic = 1
+
+# 예외적인 경우:
+# "abc_DEF" 등 중간에 밑줄 다음에 대문자가 나오는 경우
+# "translator_credits" GNOME magic
+# &amp; intltool에서 XML에서 변환하면서 unescape되지 않은 몇몇 케이스
+re_mnemonic_unlikely = re.compile('(^[^_]*[a-zA-Z]_[A-Z][^_]*$|^translator_credits.*|.*%[ds].*|.*&amp;)')
+
 error_string_no_mnemonic = u'번역문에 접근키가 없거나 두 개 이상입니다'
 error_string_wrong = u'\'%s\' vs \'%s\': 원문과 번역문의 접근키가 다릅니다'
 
@@ -22,7 +28,11 @@ def check(entry):
         # doubts if the string really contains a mnemonic
         if re_mnemonic_unlikely.match(msgid):
             return (1, '')
-        if len(msgid) > 60:                   # too long to be a label string
+        if len(msgid) > 60:
+            # too long string to be a label string
+            return (1, '')
+        if entry.references and entry.references[0].find('.schemas') >= 0:
+            # GNOME schema file
             return (1, '')
         
         letter = string.upper(mo.group(group_mnemonic))
@@ -43,9 +53,12 @@ def check(entry):
 
 if __name__ == '__main__':
     import sys
-    msgid = sys.stdin.readline()
-    msgstr = sys.stdin.readline()
-    t,e = check(msgid,msgstr)
+    class entry:
+        pass
+    entry.msgid = sys.stdin.readline()
+    entry.msgstr = sys.stdin.readline()
+    entry.references = []
+    t,e = check(entry)
     if not t:
         print e
     else:
