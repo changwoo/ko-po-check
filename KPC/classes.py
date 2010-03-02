0eb4aa5e5f211fbe16fea7fd6b2f4a65b98a0470
Changwoo Ryu <cwryu@debian.org>
2010-03-03 02:42:08 +0900

    Base classes for next-generation KPC

---
+++ KPC/classes.py
@@ -0,0 +1,64 @@
+class Error:
+    '''An individual error from a check against an entry. One check
+    usually returns list of errors or empty list if there's no error.
+    '''
+    SUCCESS = 0
+    DEPRECATED = 1
+    WARNING = 2
+    ERROR = 3
+
+    def __init__(self, message):
+        self.pos = 0
+        self.level = Error.ERROR
+        self.message = message
+
+    def message(self):
+        return message
+
+
+class BaseCheck:
+    '''Base class to check an entry from PO files. Subclasses should
+    implement check() function. check() returns list of errors if any.
+    '''
+    def check(self, entry):
+        return []
+
+
+class CheckList(BaseCheck):
+    '''Container class to hold other check classes. check() returns
+    all errors from the contained sub-checks.
+    '''
+    def __init__(self, l):
+        super(CheckList, self).__init__()
+        self.checks = l
+
+    def add(self, check):
+        self.checks.append(check)
+
+    def check(self, entry):
+        ret = []
+        for c in self.checks:
+            ret += c.check(self, entry)
+        return ret
+
+
+class HeaderCheck(BaseCheck):
+    '''Base class to check header message fields. Subclasses should
+    implement check_fields() function.
+    '''
+    def check(self, entry):
+        # header messages only
+        if entry.msgid != '':
+            return []
+        fields = {}
+        # parse msgstr
+        for line in entry.msgstr.split('\n'):
+            try:
+                k, v = line.split(': ', 1)
+                fields[k] = v
+            except ValueError:
+                pass
+        return self.check_fields(fields)
+        
+    def check_fields(self, fields):
+        return []
