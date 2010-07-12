26ee1db5b74b9ffc550bf7a2baa88105a3e16ae7
Changwoo Ryu <cwryu@debian.org>
2010-07-12 11:28:50 +0900

    mnemonic -> access key

--- KPC/checks/convention/accesskey.py
+++ KPC/checks/convention/accesskey.py
@@ -3,31 +3,31 @@
 import re, string
 from KPC.classes import Error, BaseCheck
 
-gnome_mnemonic_re = re.compile('^[^_]*_([0-9A-Za-z])[^_]*$')
-kde_mnemonic_re = re.compile('^[^_]*&([0-9A-Za-z])[^_]*$')
+gnome_accesskey_re = re.compile('^[^_]*_([0-9A-Za-z])[^_]*$')
+kde_accesskey_re = re.compile('^[^_]*&([0-9A-Za-z])[^_]*$')
 
 
-re_mnemonic = re.compile('^[^_]*[_&]([0-9A-Za-z])[^_]*$')
+re_accesskey = re.compile('^[^_]*[_&]([0-9A-Za-z])[^_]*$')
 
 # 예외적인 경우:
-# "abc_DEF" 등 중간에 밑줄 다음에 대문자가 나오는 경우
-# &amp; intltool에서 XML에서 변환하면서 unescape되지 않은 몇몇 케이스
-re_mnemonic_unlikely = re.compile('(^[^_]*[a-zA-Z]_[A-Z][^_]*$.*%[ds].*)')
+# - "abc_DEF" 등 중간에 밑줄 다음에 대문자가 나오는 경우
+# - &amp; intltool에서 XML에서 변환하면서 unescape되지 않은 몇몇 케이스
+re_accesskey_unlikely = re.compile('^[^_]*[a-zA-Z]_[A-Z][^_]*$')
 
-errstr_no_mnemonic = u'번역문에 접근키가 없거나 두 개 이상입니다'
+errstr_no_accesskey = u'번역문에 접근키가 없거나 두 개 이상입니다'
 errstr_wrong = u'\'%s\' vs \'%s\': 원문과 번역문의 접근키가 다릅니다'
 
-re_lowercase_mnemonic = re.compile('(\([_&][a-z]\))')
+re_lowercase_accesskey = re.compile('(\([_&][a-z]\))')
 errstr_lowercase = u'\"%s\": 접근키가 소문자입니다'
 
 class AccessKeyCheck(BaseCheck):
     def check(self, entry):
         msgid = entry.msgid
         msgstr = entry.msgstr
-        mo = re_mnemonic.match(msgid)
+        mo = re_accesskey.match(msgid)
         if mo:
-            # doubts if the string really contains a mnemonic
-            if re_mnemonic_unlikely.match(msgid):
+            # doubts if the string really contains a access key
+            if re_accesskey_unlikely.match(msgid):
                 return []
             # too long string to be a label string
             if len(msgid) > 60:
@@ -37,17 +37,17 @@ class AccessKeyCheck(BaseCheck):
                 return []
             
             letter = string.upper(mo.group(1))
-            # check 1: if it's translated with the mnemonic
-            mo = re_mnemonic.match(msgstr)
+            # check 1: if it's translated with the access key
+            mo = re_accesskey.match(msgstr)
             if not mo:
-                return [Error(errstr_no_mnemonic)]
+                return [Error(errstr_no_accesskey)]
 
             # check 2: check if it's same
             if letter != string.upper(mo.group(1)):
                 return [Error(errstr_wrong % (letter, mo.group(1)))]
 
             # check 3: check if it's uppercase in "(_X)" form
-            mo = re_lowercase_mnemonic.search(msgstr)
+            mo = re_lowercase_accesskey.search(msgstr)
             if mo:
                 return [Error(errstr_lowercase % mo.group(1))]
         return []
