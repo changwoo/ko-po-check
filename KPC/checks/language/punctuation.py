386c7771ec6d2ba559caa9b8f4c2b6a0127cf6e2
Changwoo Ryu <cwryu@debian.org>
2012-05-19 22:23:50 +0900

    괄호 앞에 띄어 쓴 경우에 대한 검사
    서양식은 괄호 앞에 반드시 띄어 쓰기 때문에 번역문에서도 띄어 쓰기
    쉽지만, 맞춤법 규정에 따라 우리말에서는 앞에 띄어 쓰지 않는다.

---
+++ KPC/checks/language/punctuation.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import string, re
+from KPC.classes import Error, BaseCheck
+
+# 괄호 띄어쓰기
+check_list = [
+    [ re.compile(r'\w \(\w', re.UNICODE), '맞춤법 규정에 따라 괄호 앞에 띄어 쓰지 않습니다' ]
+]
+
+class PunctuationCheck(BaseCheck):
+    def check(self, entry):
+        msgid = entry.msgid
+        msgstr = entry.msgstr
+        errors = []
+        for (r, emsg) in check_list:
+            mo = r.search(msgstr)
+            if mo:
+                errors.append(Error(emsg))
+        return errors
+
+name = 'language/punctuation'
+description = '잘못된 한국어 구문 규칙을 찾아냅니다'
+checker = PunctuationCheck()
