e4ea1888dc02fff354c23c6c08d0866721a86442
Changwoo Ryu <cwryu@debian.org>
2014-04-04 22:39:22 +0900

    패키지 메타데이터 추가

--- setup.py
+++ setup.py
@@ -10,16 +10,14 @@ from KPC.config import VERSION
 
 setup(name='ko-po-check',
       version=VERSION,
-      description='Korean PO checker',
       author='Changwoo Ryu',
       author_email='cwryu@debian.org',
       url='https://github.com/changwoo/ko-po-check',
-
-      packages = ['KPC', 'KPC/checks',
-                  'KPC/checks/language',
-                  'KPC/checks/convention',
-                  'KPC/checks/terminology' ],
-      scripts=['scripts/ko-po-check'],
+      license = 'GPLv3 or later',
+      description='Korean PO checker',
+      long_description = 'Korean message translation checker',
+      platforms = ['any'],
+      download_url='https://github.com/changwoo/ko-po-check/releases',
       classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
@@ -31,5 +29,11 @@ setup(name='ko-po-check',
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Localization',
         ],
+
+      packages = ['KPC', 'KPC/checks',
+                  'KPC/checks/language',
+                  'KPC/checks/convention',
+                  'KPC/checks/terminology' ],
+      scripts=['scripts/ko-po-check'],
       )
  
