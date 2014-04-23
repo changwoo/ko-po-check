f2543fcda87af1f5957035c43b0c4f869f1833d1
Changwoo Ryu <cwryu@debian.org>
2014-04-24 06:18:52 +0900

    새 검사 규칙 convention/desktop-keyword
    freedesktop.org desktop 엔트리의 Keyword 번역을 확인합니다

---
+++ KPC/checks/convention/desktop-keyword.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import re, string
+from KPC.classes import Error, BaseCheck
+
+err_fmt = 'freedesktop desktop의 Keyword 번역은 \';\'으로 끝나야 합니다'
+err_org = '%s: freedesktop desktop의 Keyword 번역에는 원문 키워드를 포함해야 합니다'
+
+re_keyword = re.compile(r'^(?:[\w ]+;)+$')
+
+class DesktopKeywordCheck(BaseCheck):
+    def is_keyword(self, entry):
+        if not True in [('.desktop' in p) for p in entry.references]:
+            return False
+        return re_keyword.match(entry.msgid)
+
+    def check(self, entry):
+        if not self.is_keyword(entry):
+            return []
+        msgid_keywords = entry.msgid[:-1].split(';')
+        if entry.msgstr[-1] != ';':
+            return [Error(err_fmt)]
+        msgstr_keywords = entry.msgstr[:-1].split(';')
+        for k in msgid_keywords:
+            if not k in msgstr_keywords:
+                return [Error(err_org % k)]
+        return []
+        
+name = 'convention/desktop-keyword'
+description = 'freedesktop.org desktop 엔트리의 Keyword 번역을 확인합니다'
+checker = DesktopKeywordCheck()
